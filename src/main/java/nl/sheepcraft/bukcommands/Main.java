78a19148e43093064fa792d4621373d557991c2a
Sam Schaap <samuelschaap@gmail.com>
2014-05-17 08:53:42 +0200

    Resourcedl Command

--- src/main/java/nl/sheepcraft/bukcommands/Main.java
+++ src/main/java/nl/sheepcraft/bukcommands/Main.java
@@ -6,6 +6,7 @@ import nl.sheepcraft.bukcommands.commands.BroadcastCommandExecutor;
 import nl.sheepcraft.bukcommands.commands.FlyCommandExecutor;
 import nl.sheepcraft.bukcommands.commands.GamemodeCommandExecutor;
 import nl.sheepcraft.bukcommands.commands.HealCommandExecutor;
+import nl.sheepcraft.bukcommands.commands.ResourcedlCommandExecutor;
 import nl.sheepcraft.bukcommands.commands.SkullCommandExecutor;
 import nl.sheepcraft.bukcommands.commands.SpawnCommandExecutor;
 import nl.sheepcraft.bukcommands.commands.WorkbenchCommandExecutor;
@@ -25,6 +26,7 @@ public class Main extends JavaPlugin {
         getCommand("fly").setExecutor(new FlyCommandExecutor());
         getCommand("skull").setExecutor(new SkullCommandExecutor());
         getCommand("heal").setExecutor(new HealCommandExecutor());
+        getCommand("resourcedl").setExecutor(new ResourcedlCommandExecutor());
     }
 
 }
---
+++ src/main/java/nl/sheepcraft/bukcommands/commands/ResourcedlCommandExecutor.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nl.sheepcraft.bukcommands.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+/**
+ *
+ * @author samschaap
+ */
+public class ResourcedlCommandExecutor implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("resourcedl")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage(ChatColor.RED + "Only players can use this command!");
+            } else {
+                if (args == null) {
+                    Player player = (Player) sender;
+                    player.sendMessage(ChatColor.RED + "Please follow this command by an URL.");
+                } else {
+                    Player player = (Player) sender;
+                    player.setResourcePack(args[0].toString());
+                    player.sendMessage(ChatColor.GREEN + "Resourcepack from URL " + args[0].toString() + " downloading. Make sure you have enabled resource pack downloads in your options!");
+                    Bukkit.getLogger().info(player.getName() + " downloaded resourcepack from URL " + args[0].toString());
+                }
+            }
+        }
+        return true;
+    }
+
+}
